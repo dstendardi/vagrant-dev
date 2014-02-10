fba6344c31ec8cab544161c3fcb76b394952592c
David Stendardi <david.stendardi@gmail.com>
2014-02-10 11:48:08 +0100

    initial commit

---
+++ Vagrantfile
@@ -0,0 +1,18 @@
+Vagrant.configure("2") do |config|
+  config.vm.box_url = 'http://files.vagrantup.com/precise64.box'
+  config.vm.box = "precise64"
+
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 2048
+  end
+  
+  config.vm.synced_folder ".", "/home/vagrant/src"
+
+  config.vm.provision "docker"
+  config.vm.network "forwarded_port", guest: 8080, host: 8080
+  config.vm.network "forwarded_port", guest: 3000, host: 3000
+  config.vm.network "forwarded_port", guest: 9200, host: 9200
+
+  config.vm.provision "shell", path: "ops/provision.sh"
+
+end
---
+++ provision.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+cd /tmp
+
+##
+# GENERAL 
+##
+
+# network
+apt-get install -y -q wget curl net-tools netcat tcpdump
+
+# version control
+apt-get install -y -q git
+echo '[color]
+	ui = true
+[color "branch"]
+	current = yellow bold
+	local = green bold
+	remote = cyan bold
+[color "diff"]
+	meta = yellow bold
+	frag = magenta bold
+	old = red bold
+	new = green bold
+	whitespace = red reverse
+[color "status"]
+	added = green bold
+	changed = yellow bold
+	untracked = red bold
+' > /etc/gitconfig
+cd /vagrant 
+git config branch.master.rebase true
+cd /tmp
+
+# editors
+apt-get install -y -q vim
+
+# zip/unzip
+apt-get install -y -q unzip zip
+
+# Install htop 
+apt-get install -y -q htop 
+
+# Install packages needed to compile binaries
+apt-get install -y -q python-software-properties build-essential autotools-dev automake pkg-config
+
+# scripting 
+apt-get install -y python-pip 
+
+
+##
+# ZSH
+##
+
+# Install zsh
+apt-get install -y -q zsh
+
+# Clone oh-my-zsh
+if [ ! -d ~vagrant/.oh-my-zsh ]; then
+  git clone https://github.com/robbyrussell/oh-my-zsh.git ~vagrant/.oh-my-zsh
+fi
+
+# Create a new zsh configuration from the provided template
+cp ~vagrant/.oh-my-zsh/templates/zshrc.zsh-template ~vagrant/.zshrc
+
+# Customize theme
+sed -i -e 's/ZSH_THEME=".*"/ZSH_THEME="bira"/' ~vagrant/.zshrc
+
+# add aliases
+sed -i -e 's/# Example aliases/source ~\/.bash_aliases/gi' ~vagrant/.zshrc
+
+# Set zsh as default shell
+chsh -s /bin/zsh vagrant
+
+##
+# Maestro
+##
+
+cd /tmp
+
+if [ ! -d "maestro-ng" ]; then
+  git clone https://github.com/signalfuse/maestro-ng.git
+  cd maestro-ng
+  pip install . 
+fi
+
+
+
+##
+# Docker
+##
+
+echo "DOCKER_OPTS=\"-H 127.0.0.1:4243\"" > /etc/default/docker
+service docker restart
+sleep 2
+docker -H 127.0.0.1:4243 build -t rethinkdb /vagrant/ops/docker/rethinkdb/
+
+##
+# Nodejs
+##
+
+apt-add-repository ppa:chris-lea/node.js-devel
+apt-get update
+apt-get install -y nodejs
+
+echo "
+alias m='python -m maestro'
+alias docker='docker -H 127.0.0.1:4243'
+alias node='node --harmony'" > /home/vagrant/.bash_aliases
+
+
